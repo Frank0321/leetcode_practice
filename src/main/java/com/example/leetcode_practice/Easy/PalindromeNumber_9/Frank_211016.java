4131a83ca462bb53a97ee4ee73dc35c947a5863a
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-10-16 09:14:41 +0800

    補充 9 的註解

--- src/main/java/com/example/leetcode_practice/Easy/PalindromeNumber_9/Frank_211016.java
+++ src/main/java/com/example/leetcode_practice/Easy/PalindromeNumber_9/Frank_211016.java
@@ -5,10 +5,16 @@ import java.util.List;
 
 public class Frank_211016 {
     public static void main(String[] args) {
-        System.out.println(isPalindrome(1));
+        System.out.println(isPalindrome(1231));
 
     }
 
+    /**
+     * method :
+     * 1. 先判斷數字是否為正數，負數一定為 false
+     * 2. 將數字從後面一個個拆解，並裝到 List 中，但需要注意最後一個數字是否有裝進去 list 中 (順序顛屌)
+     * 3. 將前後成對的 list 比對，如果不一樣就是 false，可以比對到最後。就拋出 true
+     */
     public static boolean isPalindrome(int x) {
         if (x < 0){
             return false;
