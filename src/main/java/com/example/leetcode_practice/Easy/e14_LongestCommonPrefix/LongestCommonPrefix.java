7734f76aee17d2e45a476400d70b8a40f887d37c
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-10-20 21:55:49 +0800

    Easy 14 : Longest Common Prefix

---
+++ src/main/java/com/example/leetcode_practice/Easy/e14_LongestCommonPrefix/LongestCommonPrefix.java
@@ -0,0 +1,34 @@
+package com.example.leetcode_practice.Easy.e14_LongestCommonPrefix;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String [] strings = new String[]{"flower","flow","flight"};
+        System.out.println(LongestCommon(strings));
+    }
+
+    public static String LongestCommon (String[] strs){
+        //取出最短的文字
+        var firstStr = Arrays.stream(strs).sorted(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        }).findFirst().orElse("");
+
+
+        for (int i = 0; i <= firstStr.length(); i++) {
+            String check = firstStr.substring(0,i);
+            for (String str : strs) {
+                if(!check.equals(str.substring(0, i))){
+                    return firstStr.substring(0,i-1);
+                }
+            }
+        }
+        return firstStr;
+    }
+}
