b9cc72b09afca6d52c7f4d8cf9b4333c0bf328f5
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-10-22 00:49:19 +0800

    Easy 53 : 準備要寫

---
+++ src/main/java/com/example/leetcode_practice/Easy/e53_MaximumSubarray/Frank_211022.java
@@ -0,0 +1,15 @@
+package com.example.leetcode_practice.Easy.e53_MaximumSubarray;
+
+public class Frank_211022 {
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{5,4,-1,7,8};
+        System.out.println(maxSubArray(nums));
+    }
+
+    public static int maxSubArray(int[] nums){
+        int result = 0;
+
+        return result;
+    }
+}
