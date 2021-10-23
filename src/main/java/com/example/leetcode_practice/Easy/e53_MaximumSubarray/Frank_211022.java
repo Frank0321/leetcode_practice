f9d16aff5c8abf2f587749b1d9675f0ac9188c3a
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-10-24 00:35:22 +0800

    Easy 53 : 補充註解

--- src/main/java/com/example/leetcode_practice/Easy/e53_MaximumSubarray/Frank_211022.java
+++ src/main/java/com/example/leetcode_practice/Easy/e53_MaximumSubarray/Frank_211022.java
@@ -9,15 +9,16 @@ public class Frank_211022 {
         System.out.println(maxSubArray(nums));
     }
 
+    /**
+     * 如果"累加的數字(sum)"小於 0，就從下一個數字開始"重新進行累加"(sum = num)，否則就進行累加
+     * 如果累加的數字大於結果(result)的數字，就取代它
+     */
     public static int maxSubArray(int[] nums){
         int result = Integer.MIN_VALUE;
         int sum = 0;
 
         for (int num : nums) {
-            //如果"累加的數字"小於 0，就從下一個數字開始"重新進行累加"
             sum = sum < 0 ? num : sum+num;
-
-            //如果累加的數字大於結果的數字，就取代它
             result = result < sum ? sum : result;
         }
 
