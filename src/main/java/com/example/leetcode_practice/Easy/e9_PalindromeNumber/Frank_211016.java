fa407a67f293d18d4ded6ec0248c86e909bbeb81
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-10-31 15:02:47 +0800

    Easy 9 : 新增其他寫法 (一行)

--- src/main/java/com/example/leetcode_practice/Easy/e9_PalindromeNumber/Frank_211016.java
+++ src/main/java/com/example/leetcode_practice/Easy/e9_PalindromeNumber/Frank_211016.java
@@ -16,22 +16,32 @@ public class Frank_211016 {
      * 3. 將前後成對的 list 比對，如果不一樣就是 false，可以比對到最後。就拋出 true
      */
     public static boolean isPalindrome(int x) {
-        if (x < 0){
-            return false;
-        }else {
-            List<Integer> list = new ArrayList<Integer>();
-            while (x/10 >= 1){
-                list.add(x%10);
-                x /= 10;
-            }
-            list.add(x);
-            for(int i = 0; i< list.size()/2 ; i++){
-                if (list.get(i) != list.get(list.size()-1-i)){
-                    return false;
-                }
-            }
-            return true;
-        }
+//        if (x < 0){
+//            return false;
+//        }else {
+//            List<Integer> list = new ArrayList<Integer>();
+//            while (x/10 >= 1){
+//                list.add(x%10);
+//                x /= 10;
+//            }
+//            list.add(x);
+//            for(int i = 0; i< list.size()/2 ; i++){
+//                if (list.get(i) != list.get(list.size()-1-i)){
+//                    return false;
+//                }
+//            }
+//            return true;
+//        }
+
+//        if(x < 0){
+//            return false;
+//        }else {
+//            return (new StringBuilder().append(x).reverse().toString())
+//                    .equals(new StringBuilder().append(x).toString());
+//        }
+        //interillj 縮減的
+        return x >= 0 && (new StringBuilder().append(x).toString())
+                .equals(new StringBuilder().append(x).reverse().toString());
 
     }
 }
