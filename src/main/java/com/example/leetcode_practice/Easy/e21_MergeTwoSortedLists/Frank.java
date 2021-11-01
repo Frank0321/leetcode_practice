5c595734fcb02eaedde6884f3472276528ff854a
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-11-01 22:48:52 +0800

    easy 21 : 撰寫題目

---
+++ src/main/java/com/example/leetcode_practice/Easy/e21_MergeTwoSortedLists/Frank.java
@@ -0,0 +1,14 @@
+package com.example.leetcode_practice.Easy.e21_MergeTwoSortedLists;
+
+/**
+ * FileName : Frank
+ * CreatTime : 2021/11/1
+ * Author : Frank.Chang
+ * Description :
+ */
+public class Frank {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(4, new ListNode(2, new ListNode(1)));
+        ListNode l2 = new ListNode(4, new ListNode(3, new ListNode(1)));
+    }
+}
---
+++ src/main/java/com/example/leetcode_practice/Easy/e21_MergeTwoSortedLists/ListNode.java
@@ -0,0 +1,22 @@
+package com.example.leetcode_practice.Easy.e21_MergeTwoSortedLists;
+
+/**
+ * FileName : ListNode
+ * CreatTime : 2021/11/1
+ * Author : Frank.Chang
+ * Description :
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(){
+
+    }
+    ListNode(int val){
+        this.val = val;
+    }
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
