e1c59a7d55c72faf132c08a92325979680c728b3
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2022-01-07 16:34:25 +0800

    新增 String null 的判斷

--- src/main/java/com/example/leetcode_practice/other/StringTest/StringTest.java
+++ src/main/java/com/example/leetcode_practice/other/StringTest/StringTest.java
@@ -12,7 +12,7 @@ public class StringTest {
         String B = "test";
         String C = new String("test");
 
-        System.out.println(A == B);       //t
+        System.out.println(A == B);
         System.out.println(A == C);       //f
         System.out.println(B == C);       //f
         System.out.println("-----");
@@ -24,8 +24,19 @@ public class StringTest {
         System.out.println(B == "test");  //t
         System.out.println(C == "test");  //f
         System.out.println("-----");
-        System.out.println(A.equals("test"));  //t
-        System.out.println(B.equals("test"));  //t
-        System.out.println(C.equals("test"));  //t
+        System.out.println(A.equals("test"));
+        System.out.println(B.equals("test"));
+        System.out.println(C.equals("test"));
+
+        String D = null;
+        String E = "null";
+
+        try {
+            System.out.println(D.equals(E));
+        }catch (NullPointerException e){
+            System.out.println("Exception");
+        }finally {
+            System.out.println("finally");
+        }
     }
 }
