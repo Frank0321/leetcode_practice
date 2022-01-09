e5de599f1b92f12614a1ddbb07f3362e04dc5d36
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2022-01-10 00:18:38 +0800

    ArrayList、LinkedList 比較

---
+++ src/main/java/com/example/leetcode_practice/other/CollectionInfo/CollectionClass.java
@@ -0,0 +1,22 @@
+package com.example.leetcode_practice.other.CollectionInfo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * FileName : CollectionClass
+ * CreatTime : 2022/1/9
+ * Author : Frank.Chang
+ * Description :
+ */
+public class CollectionClass {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        arrayList.add(null);
+        linkedList.add(null);
+
+
+    }
+}
